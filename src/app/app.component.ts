6235ae453a942b568db6bdb491bfdefd92d6427a
Kirill.Kolomin <kirill.kolomin@sidenis.com>
2019-12-28 15:34:37 +0700

    Made a form for a cookies playground

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
+
+enum CookieActions {
+  delete = 'delete',
+  get = 'get',
+  set = 'set'
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'cookies';
+export class AppComponent implements OnInit {
+  cookiesFormGroup: FormGroup;
+
+  cookieActionOptions = Object.keys(CookieActions);
+
+  constructor(public formBuilder: FormBuilder) {
+  }
+
+  ngOnInit(): void {
+    this.cookiesFormGroup = this.formBuilder.group({
+      action: [CookieActions.set],
+      name: [''],
+      value: ['']
+    });
+  }
+
+  onSubmit(): void {
+    console.log(this.cookiesFormGroup);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatSelectModule} from '@angular/material/select';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -10,9 +15,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
